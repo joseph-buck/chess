051502adc13ce3f137c44f122724850c53e4dea1
Joseph Buck <joeypiano1998@gmail.com>
2023-10-05 02:18:07 -0600

    Cleaned up code and added comments in Game.java

--- src/starter/chess/Game.java
+++ src/starter/chess/Game.java
@@ -28,49 +28,61 @@ public class Game implements ChessGame {
         this.teamTurn = team;
     }
 
+    /**
+     * Description in ChessGame.java
+     */
     @Override
     public Collection<ChessMove> validMoves(ChessPosition startPosition) {
+        Collection<ChessMove> grossMoves = getGrossMoves(startPosition);
+        Collection<ChessMove> validMoves = getAllLegalMoves(grossMoves,
+                board.getPiece(startPosition).getTeamColor());
+        return validMoves;
+    }
+
+    /**
+     * Function to get all possible moves from a given position without taking
+     * into account moves into check.
+     * @param startPosition : The location of the piece to be moved
+     * @return grossMoves : The set of possible moves
+     */
+    private Collection<ChessMove> getGrossMoves(ChessPosition startPosition) {
         Collection<ChessMove> grossMoves = new HashSet<>();
-        Collection<ChessMove> validMoves = new HashSet<>();
 
         ChessPiece myPiece = board.getPiece(startPosition);
         if (myPiece != null) {
             grossMoves = new HashSet<>(myPiece.pieceMoves(board, startPosition));
         }
+        return grossMoves;
+    }
 
-
-
+    /**
+     * Generates a set of legal moves, taking moves into check into account.
+     * @param grossMoves : Set of possible moves prior to considering check.
+     * @param teamColor : Color of team whose piece will be moved
+     * @return
+     */
+    private Collection<ChessMove> getAllLegalMoves(Collection<ChessMove> grossMoves,
+                                                   TeamColor teamColor) {
+        Collection<ChessMove> finalMoves = new HashSet<>();
 
         for (ChessMove move : grossMoves) {
+            ChessPosition startPosition = move.getStartPosition();
             ChessPosition endPosition = move.getEndPosition();
             ChessPiece sourcePiece = board.getPiece(startPosition);
             ChessPiece destinationPiece = board.getPiece(endPosition);
 
             carryOutMove(move, sourcePiece);
-            if (!isInCheck(sourcePiece.getTeamColor())) {
-                validMoves.add(move);
+            if (!isInCheck(teamColor)) {
+                finalMoves.add(move);
             }
             backTrackMove(move, sourcePiece, destinationPiece);
         }
-
-        System.out.println(validMoves);
-        //validMoves = grossMoves;//new HashSet<ChessMove>(getAllLegalMoves(grossMoves, myPiece.getTeamColor()));
-        return validMoves;
+        return finalMoves;
     }
 
-    private Collection<ChessMove> validMovesTEMP(ChessPosition startPosition) {
-        Collection<ChessMove> grossMoves = new HashSet<>();
-        Collection<ChessMove> validMoves = new HashSet<>();
-
-        ChessPiece myPiece = board.getPiece(startPosition);
-        if (myPiece != null) {
-            grossMoves = new HashSet<>(myPiece.pieceMoves(board, startPosition));
-        }
-        //System.out.println(new HashSet<ChessMove>(getAllLegalMoves(grossMoves, myPiece.getTeamColor())));
-        validMoves = grossMoves;//new HashSet<ChessMove>(getAllLegalMoves(grossMoves, myPiece.getTeamColor()));
-        return validMoves;
-    }
-
+    /**
+     * Description in ChessGame.java
+     */
     @Override
     public void makeMove(ChessMove move) throws InvalidMoveException {
         ChessPosition startPosition = move.getStartPosition();
@@ -78,7 +90,7 @@ public class Game implements ChessGame {
         ChessPiece sourcePiece = board.getPiece(startPosition);
         ChessPiece destinationPiece = board.getPiece(endPosition);
 
-        if (!validMovesTEMP(startPosition).contains(move)) {
+        if (!getGrossMoves(startPosition).contains(move)) {
             throw new InvalidMoveException(String.format(
                     "Move %s is not a valid move", move));
         }
@@ -96,13 +108,16 @@ public class Game implements ChessGame {
         this.teamTurn = (this.teamTurn == WHITE) ? BLACK : WHITE;
     }
 
+    /**
+     * Performs a move. Handles details of pawn promotion.
+     * @param move : The move to be carried out
+     * @param sourcePiece : The piece to be moved
+     */
     private void carryOutMove(ChessMove move, ChessPiece sourcePiece) {
         ChessPiece movingPiece = sourcePiece;
         ChessPiece.PieceType promotionPieceType = move.getPromotionPiece();
         TeamColor teamColor = sourcePiece.getTeamColor();
         if (move.getPromotionPiece() != null) {
-            System.out.println("INSIDE PROMOTION");
-            System.out.println(move.getPromotionPiece());
             switch (promotionPieceType) {
                 case ROOK:      movingPiece = new Rook(teamColor);
                                 break;
@@ -119,13 +134,24 @@ public class Game implements ChessGame {
         board.removePiece(move.getStartPosition());
     }
 
+    /**
+     * Undoes a move in the even it needs to be undone.
+     * @param move : The move to be undone
+     * @param sourcePiece : The piece to return to its original location
+     * @param destinationPiece : The piece that was captured (it
+     */
     private void backTrackMove(ChessMove move, ChessPiece sourcePiece,
                                ChessPiece destinationPiece) {
         board.addPiece(move.getStartPosition(), sourcePiece);
         board.removePiece(move.getEndPosition());
-        board.addPiece(move.getEndPosition(), destinationPiece);
+        if (destinationPiece != null) {
+            board.addPiece(move.getEndPosition(), destinationPiece);
+        }
     }
 
+    /**
+     * Description in ChessGame.java
+     */
     @Override
     public boolean isInCheck(TeamColor teamColor) {
         ChessPosition kingPosition = findKing(teamColor);
@@ -136,7 +162,7 @@ public class Game implements ChessGame {
                 ChessPosition currPosition = new Position(i, j);
                 ChessPiece currPiece = board.getPiece(currPosition);
                 if ((currPiece != null) && (currPiece.getTeamColor() != teamColor)) {
-                    validMoves = validMovesTEMP(currPosition);
+                    validMoves = getGrossMoves(currPosition);
                     for (ChessMove move : validMoves) {
                         if (move.getEndPosition().equals(kingPosition)) {
                             return true;
@@ -148,6 +174,11 @@ public class Game implements ChessGame {
         return false;
     }
 
+    /**
+     * Finds the position of the king of the given team.
+     * @param teamColor : Color of team whose king will be found
+     * @return currPosition : The position of the king
+     */
     private ChessPosition findKing(TeamColor teamColor) {
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
@@ -162,6 +193,9 @@ public class Game implements ChessGame {
         return null;
     }
 
+    /**
+     * Description in ChessGame.java
+     */
     @Override
     public boolean isInCheckmate(TeamColor teamColor) {
         Collection<ChessMove> validMoves = getTeamValidMoves(teamColor);
@@ -174,6 +208,9 @@ public class Game implements ChessGame {
         }
     }
 
+    /**
+     * Description in ChessGame.java
+     */
     @Override
     public boolean isInStalemate(TeamColor teamColor) {
         Collection<ChessMove> validMoves = getTeamValidMoves(teamColor);
@@ -184,25 +221,12 @@ public class Game implements ChessGame {
         return (finalMoves.isEmpty()) && (this.teamTurn == teamColor);
     }
 
-    private Collection<ChessMove> getAllLegalMoves(Collection<ChessMove> validMoves,
-                                                   TeamColor teamColor) {
-        Collection<ChessMove> finalMoves = new HashSet<>();
-
-        for (ChessMove move : validMoves) {
-            ChessPosition startPosition = move.getStartPosition();
-            ChessPosition endPosition = move.getEndPosition();
-            ChessPiece sourcePiece = board.getPiece(startPosition);
-            ChessPiece destinationPiece = board.getPiece(endPosition);
-
-            carryOutMove(move, sourcePiece);
-            if (!isInCheck(teamColor)) {
-                finalMoves.add(move);
-            }
-            backTrackMove(move, sourcePiece, destinationPiece);
-        }
-        return finalMoves;
-    }
-
+    /**
+     * Creates a set of all moves that a given team can execute, without taking
+     * check into account. That is handled later by getAllLegalMoves.
+     * @param teamColor : The team whose moves will be compiled.
+     * @return moves : Set of moves
+     */
     private Collection<ChessMove> getTeamValidMoves(TeamColor teamColor) {
         Collection<ChessMove> moves = new HashSet<>();
 
@@ -211,18 +235,24 @@ public class Game implements ChessGame {
                 ChessPosition currPosition = new Position(i, j);
                 ChessPiece currPiece = board.getPiece(currPosition);
                 if ((currPiece != null) && (currPiece.getTeamColor() == teamColor)) {
-                    moves.addAll(validMovesTEMP(currPosition));
+                    moves.addAll(getGrossMoves(currPosition));
                 }
             }
         }
         return moves;
     }
 
+    /**
+     * Description in ChessGame.java
+     */
     @Override
     public void setBoard(ChessBoard board) {
         this.board = (Board)board;
     }
 
+    /**
+     * Description in ChessGame.java
+     */
     @Override
     public ChessBoard getBoard() {
         return this.board;
